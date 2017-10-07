3809c82884414ad5b81580def8b7420786b432cc
Prajwal Nayak <theprajwalnayak@gmail.com>
2017-10-08 01:00:35 +0530

    Adding tech and sports

---
+++ app/scripts/controllers/tech.js
@@ -0,0 +1,65 @@
+'use strict'
+
+
+angular.module('TheNayakNews').controller('techController', ['$scope', 'webService', function ($scope, webService) {
+
+  var apiKey = "74c20057e29e4641a38b5d00c39bd93f";
+  var filmRightUrl = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=" + apiKey;
+  var filmLeftUrl = "https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=" + apiKey;
+  initialize();
+
+  function initialize() {
+    prepareSportsLeftData(filmLeftUrl);
+    prepareSportsRightData(filmRightUrl);
+  }
+
+  function prepareSportsLeftData(url) {
+    var res = webService.getData(url);
+    res.then(function (response) {
+        $scope.sportsLeftArticles = [];
+        if (angular.isDefined(response.data.articles)) {
+          angular.forEach(response.data.articles, function (value, key) {
+            var filmNews = {
+              author: value.author,
+              title: value.title,
+              description: value.description,
+              url: value.url,
+              urlToImage: value.urlToImage,
+              publishedAt: value.publishedAt
+            }
+            $scope.sportsLeftArticles.push(filmNews);
+          })
+        }
+      },
+      function (response) {
+
+      });
+  }
+
+  function prepareSportsRightData(url) {
+    var res = webService.getData(url);
+    res.then(function (response) {
+        $scope.sportsRightArticles = [];
+        if (angular.isDefined(response.data.articles)) {
+          angular.forEach(response.data.articles, function (value, key) {
+            var filmNews = {
+              author: value.author,
+              title: value.title,
+              description: value.description,
+              url: value.url,
+              urlToImage: value.urlToImage,
+              publishedAt: value.publishedAt
+            }
+            $scope.sportsRightArticles.push(filmNews);
+          })
+        }
+      },
+      function (response) {
+
+      });
+  }
+
+  $scope.redirectToOriginalSite = function (url) {
+    window.open(url);
+  }
+}]);
