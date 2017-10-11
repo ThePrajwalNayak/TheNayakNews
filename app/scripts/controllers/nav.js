fe5fb13d96829f27f76859e90a50b1f47a925b20
Prajwal Nayak <theprajwalnayak@gmail.com>
2017-10-11 06:12:45 +0530

    Adding fav icon and active class to nav menu

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,8 @@
+'use strict'
+
+
+angular.module('TheNayakNews').controller('navController', ['$scope', '$location', function ($scope, $location)  {
+    $scope.isCurrentPath = function (path) {
+      return $location.path() == path;
+    };
+}]);
