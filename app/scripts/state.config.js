0fddda471aec41df5a9c1f63491c9ce69d1433a8
Prajwal Nayak <theprajwalnayak@gmail.com>
2017-10-26 01:57:28 +0530

    removing hasbang using locationProvider

--- app/scripts/state.config.js
+++ app/scripts/state.config.js
@@ -1,27 +1,28 @@
 'use strict';
-angular.module('TheNayakNews').config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+angular.module('TheNayakNews').config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
     // For any unmatched url, send to /business
     $urlRouterProvider.otherwise("/");
 
     $stateProvider
         .state('home', {//State demonstrating Nested views
             url: "/",
-           templateUrl: "views/home.html"
+            templateUrl: "views/home.html"
         })
         .state('tech', {//State demonstrating Nested views
             url: "/tech",
-           templateUrl: "views/tech.html",
-           controller: 'techController'
+            templateUrl: "views/tech.html",
+            controller: 'techController'
         })
         .state('film', {//State demonstrating Nested views
             url: "/film",
-           templateUrl: "views/film.html",
-           controller: 'filmController'
+            templateUrl: "views/film.html",
+            controller: 'filmController'
         })
         .state('sports', {//State demonstrating Nested views
             url: "/sports",
-           templateUrl: "views/sports.html",
-           controller: 'sportsController'
+            templateUrl: "views/sports.html",
+            controller: 'sportsController'
         });
-        
+    $locationProvider.html5Mode(true);
+
 }]);
